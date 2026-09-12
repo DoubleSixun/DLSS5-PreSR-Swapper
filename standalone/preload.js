c925d288e649c893f6f7a70c46805caeeab47c7a
SJ <97835606+DoubleSixun@users.noreply.github.com>
2026-09-13 00:39:12 +1000

    feat: expose game rescan action

--- standalone/preload.js
+++ standalone/preload.js
@@ -4,15 +4,16 @@ const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('nrApp', Object.freeze({
   getState: () => ipcRenderer.invoke('app:get-state'),
-  setLanguage: (language) => ipcRenderer.invoke('app:set-language', language),
+  setLanguage: language => ipcRenderer.invoke('app:set-language', language),
+  rescanGames: () => ipcRenderer.invoke('games:rescan'),
   addGame: () => ipcRenderer.invoke('games:add'),
-  selectGame: (id) => ipcRenderer.invoke('games:select', id),
-  removeGame: (id) => ipcRenderer.invoke('games:remove', id),
-  openGameFolder: (id) => ipcRenderer.invoke('game:open-folder', id),
+  selectGame: id => ipcRenderer.invoke('games:select', id),
+  removeGame: id => ipcRenderer.invoke('games:remove', id),
+  openGameFolder: id => ipcRenderer.invoke('game:open-folder', id),
   setGameSettings: (id, settings) => ipcRenderer.invoke('game:set-settings', id, settings),
-  importRuntime: (id) => ipcRenderer.invoke('runtime:import', id),
-  install: (id) => ipcRenderer.invoke('game:install', id),
-  restore: (id) => ipcRenderer.invoke('game:restore', id),
+  importRuntime: id => ipcRenderer.invoke('runtime:import', id),
+  install: id => ipcRenderer.invoke('game:install', id),
+  restore: id => ipcRenderer.invoke('game:restore', id),
   minimize: () => ipcRenderer.send('window:minimize'),
   maximize: () => ipcRenderer.send('window:maximize'),
   close: () => ipcRenderer.send('window:close')
