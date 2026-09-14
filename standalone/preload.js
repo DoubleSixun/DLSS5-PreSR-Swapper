884de9bfe9255abc62912244c2d80c7a3a757258
DoubleSixun <97835606+DoubleSixun@users.noreply.github.com>
2026-09-14 23:30:20 +1000

    Expose in-game overlay language sync

--- standalone/preload.js
+++ standalone/preload.js
@@ -5,6 +5,7 @@ const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('nrApp', Object.freeze({
   getState: () => ipcRenderer.invoke('app:get-state'),
   setLanguage: language => ipcRenderer.invoke('app:set-language', language),
+  setOverlayLanguage: language => ipcRenderer.invoke('app:set-overlay-language', language),
   getOverlayPreferences: () => ipcRenderer.invoke('app:get-overlay-preferences'),
   setOverlayPreferences: preferences => ipcRenderer.invoke('app:set-overlay-preferences', preferences),
   rescanGames: () => ipcRenderer.invoke('games:rescan'),
